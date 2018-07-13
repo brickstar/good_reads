bcfe26ba7ac539de67baadfb96b9bd35df905727
brickstar <brickstar@gmail.com>
2018-07-13 10:16:39 -0600

    added books controller

--- spec/features/user_sees_all_books_spec.rb
+++ spec/features/user_sees_all_books_spec.rb
@@ -5,8 +5,8 @@ describe 'user visits book index' do
     before(:each) do
       @book1 = Book.create!(title: 'Little Blue Truck')
       @book2 = Book.create!(title: 'Goodnight Moon')
-      @user_1 = @book.users.create!(name: 'Pearl')
-      @user_2 = @book.users.create!(name: 'Heidi')
+      @user_1 = @book1.users.create!(name: 'Pearl')
+      @user_2 = @book2.users.create!(name: 'Heidi')
       @review_1 = @book1.reviews.create!(body: 'loved it', rating: 5, user_id: @user_1.id)
       @review_2 = @book1.reviews.create!(body: 'hated it', rating: 1, user_id: @user_2.id)
       @review_3 = @book2.reviews.create!(body: 'hated it', rating: 1, user_id: @user_2.id)
